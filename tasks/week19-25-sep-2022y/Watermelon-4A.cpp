d7668f1ec215147c9df2b423e054c63f412c25cc
Wanttegh <92159425+Wanttegh@users.noreply.github.com>
2022-09-22 21:20:31 +0300

    Create Watermelon-4A.cpp

---
+++ tasks/week19-25-sep-2022y/Watermelon-4A.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+    int w, k, counter = 0; std::cin >> w;
+    for (int i = 1; i < w; i++) {
+        k = w - i;
+        if (k % 2 == 0 && i % 2 == 0) counter++;
+        else if (i == w - 1 && i % 2 != 0) counter += 0;
+    }
+    if (counter > 0) std::cout << "YES";
+    else std::cout << "NO";
+    return 0;
+}
+
+// problem link --> https://codeforces.com/contest/4/problem/A
