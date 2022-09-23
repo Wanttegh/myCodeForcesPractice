9b599771e9750bb5d74ee47952fb4dd4f321ab46
Wanttegh <92159425+Wanttegh@users.noreply.github.com>
2022-09-23 18:44:52 +0300

    Create Given_Length_and_Sum_of_Digits-489C.cpp

---
+++ tasks/week19-25-sep-2022y/Given_Length_and_Sum_of_Digits-489C.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+int main() {
+    int m, s, i, mint; std::cin >> m >> s;
+    if ((m > 1 && s == 0) || (s > m * 9)) { std::cout << "-1 -1"; return 0; }
+    for (i = m - 1, mint = s; i >= 0; i--) {
+        int minl = std::max(0, mint-9*i);
+        if (minl == 0 && i == m - 1 && mint) {
+            minl = 1;
+        }
+        std::cout << minl;
+        mint -= minl;
+    }
+    std::cout << " ";
+    for (i = m - 1, mint = s; i >= 0; i--) {
+        int maxl = std::min(9, mint);
+        std::cout << maxl;
+        mint -= maxl;
+    }
+    return 0;
+}
+
+// problem link --> https://codeforces.com/contest/489/problem/C?locale=ru
